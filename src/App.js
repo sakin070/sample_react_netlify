4afe0245222cfb34764affaf8e777c570fa0e3f1
Saheed Akinbile <sakin070@uottawa.ca>
2020-06-29 18:39:11 -0400

    form implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <form name="contact" method="post">
+        <input type="hidden" name="form-name" value="contact" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <label>Your Name: <input type="text" name="name" /></label>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Your Role: <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </div>
   );
 }
